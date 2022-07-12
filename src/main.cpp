dee232eafc3b77da671944bbf30ebb324f1b426b
ChaseParate <97500441+ChaseParate@users.noreply.github.com>
2022-07-12 01:05:11 -0700

    Create separate screen for Pokétch display

--- src/main.cpp
+++ src/main.cpp
@@ -2,39 +2,49 @@
 
 int main()
 {
-    const int displayWidth = 256;
-    const int displayHeight = 192;
+    const int gameWidth = 256;
+    const int gameHeight = 192;
+
+    const int poketchWidth = 192;
+    const int poketchHeight = 160;
+    const Vector2 poketchOffset = {16, 16};
 
     const int windowScale = 3;
-    const int windowWidth = displayWidth * windowScale;
-    const int windowHeight = displayHeight * windowScale;
-
-    const Vector2 screenOffset = {16, 16};
+    const int windowWidth = gameWidth * windowScale;
+    const int windowHeight = gameHeight * windowScale;
 
     InitWindow(windowWidth, windowHeight, "Pokétch");
     SetTargetFPS(60);
 
-    RenderTexture2D window = LoadRenderTexture(displayWidth, displayHeight);
+    RenderTexture2D gameScreen = LoadRenderTexture(gameWidth, gameHeight);
+    RenderTexture2D poketchScreen = LoadRenderTexture(poketchWidth, poketchHeight);
 
-    Texture2D border = LoadTexture("assets/border_plat.png");
-    Texture2D screen = LoadTexture("assets/screen.png");
+    const Color backgroundColor = {112, 176, 112, 255};
+    const Color foregroundColor = {56, 80, 48, 255};
+    const Texture2D borderTexture = LoadTexture("assets/border_plat.png");
 
     while (!WindowShouldClose())
     {
         BeginDrawing();
 
-        BeginTextureMode(window);
-        ClearBackground(BLACK);
-        DrawTexture(border, 0, 0, WHITE);
-        DrawTextureV(screen, screenOffset, WHITE);
+        BeginTextureMode(poketchScreen);
+        ClearBackground(backgroundColor);
+        DrawText("Hello, Pokétch!", 20, 70, 20, foregroundColor);
         EndTextureMode();
 
-        DrawTexturePro(window.texture, {0, 0, displayWidth, -displayHeight}, {0, 0, windowWidth, windowHeight}, {0, 0}, 0, WHITE);
+        BeginTextureMode(gameScreen);
+        ClearBackground(BLACK);
+        DrawTexture(borderTexture, 0, 0, WHITE);
+        DrawTexturePro(poketchScreen.texture, {0, 0, poketchWidth, -poketchHeight}, {poketchOffset.x, poketchOffset.y, poketchWidth, poketchHeight}, {0, 0}, 0, WHITE);
+        EndTextureMode();
+
+        DrawTexturePro(gameScreen.texture, {0, 0, gameWidth, -gameHeight}, {0, 0, windowWidth, windowHeight}, {0, 0}, 0, WHITE);
 
         EndDrawing();
     }
 
-    UnloadRenderTexture(window);
+    UnloadRenderTexture(gameScreen);
+    UnloadRenderTexture(poketchScreen);
     CloseWindow();
 
     return 0;
