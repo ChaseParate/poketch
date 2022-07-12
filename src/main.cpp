9aeb773680a5ee2e1052e0c80c6fd7040cbba5ed
ChaseParate <97500441+ChaseParate@users.noreply.github.com>
2022-07-11 22:23:27 -0700

    Show some assets on the screen

--- src/main.cpp
+++ src/main.cpp
@@ -9,18 +9,24 @@ int main()
     const int windowWidth = displayWidth * windowScale;
     const int windowHeight = displayHeight * windowScale;
 
+    const Vector2 screenOffset = {16, 16};
+
     InitWindow(windowWidth, windowHeight, "Pokétch");
     SetTargetFPS(60);
 
     RenderTexture2D window = LoadRenderTexture(displayWidth, displayHeight);
+
+    Texture2D border = LoadTexture("assets/border_plat.png");
+    Texture2D screen = LoadTexture("assets/screen.png");
 
     while (!WindowShouldClose())
     {
         BeginDrawing();
 
         BeginTextureMode(window);
-        ClearBackground(RAYWHITE);
-        DrawText("Hello World!", 0, 0, 20, LIGHTGRAY);
+        ClearBackground(BLACK);
+        DrawTexture(border, 0, 0, WHITE);
+        DrawTextureV(screen, screenOffset, WHITE);
         EndTextureMode();
 
         DrawTexturePro(window.texture, {0, 0, displayWidth, -displayHeight}, {0, 0, windowWidth, windowHeight}, {0, 0}, 0, WHITE);
